1eced4da58069d58cab917c9fe33d33890e88bc5
Thanapon Yalan <thanaponyalan@gmail.com>
2020-10-13 17:55:01 +0100

    Backend

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,6 +1,19 @@
 const { temp_rtc } = require('../models/temp_rtc.model');
-exports.render=(req,res)=>{
+
+var render=(req,res)=>{
     temp_rtc.find({},(err,data)=>{
-	if(!err)res.json(data);;
+	if(!err)res.json(data);
     });
 }
+
+var addData=(req,res)=>{
+    var r=temp_rtc.insertMany(req.body);
+    res.json(r)
+}
+
+module.exports={
+    render,
+    addData
+}
+
+
--- app/routes/index.route.js
+++ app/routes/index.route.js
@@ -1,4 +1,4 @@
 module.exports=(app)=>{
     var index=require('../controllers/index.controller');
-    app.get('/',index.render);
+    app.get('/',index.render).post('/addData',index.addData);
 }
