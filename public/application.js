fc0395641e41340d45a2a6d5aaa987bcfa85c535
thanaponyalan <thanaponyalan@gmail.com>
2020-10-14 15:40:37 +0700

    angular

--- public/application.js
+++ public/application.js
@@ -1,6 +1,10 @@
 `use strict`;
 
 var mainAppModule = angular.module('Hello',[]);
+mainAppModule.controller('NameController',['$scope',function($scope){
+    $scope.yourName='No Name';
+}]);
+
 mainAppModule.filter('sayHello',function(){
     return function(name){
         return 'Hello, '+name;
