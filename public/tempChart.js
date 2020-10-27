31b4ae055e23b89af1e88ece387ab10df43aa7a2
Thanapon Yalan <thanaponyalan@Thanapons-MacBook-Pro.local>
2020-10-28 00:15:03 +0700

    trying

--- public/tempChart.js
+++ public/tempChart.js
@@ -12,35 +12,32 @@ angular.module("app", ["chart.js"])
     });
 }])
 .controller("LineCtrl", ['$scope', '$interval', '$http', function ($scope, $interval, $http) {
-    $scope.labels = ["15/10", "16/10", "17/10", "18/10", "19/10", "20/10", "21/10"];
+    /*$scope.labels = ["15/10", "16/10", "17/10", "18/10", "19/10", "20/10", "21/10"];
     $scope.series = ['Temperature', 'Humidity'];
     $scope.data = [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
-    ];
+    ];*/
+    refresh();
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
     };
 
-    var refresh;
-    $scope.sRefresh=()=>{
-        if(angular.isDefined(refresh))return;
-        refresh=$interval(()=>{
-            $http.get('http://raspberrypi.local:3009/data').success((data, status, headers, config)=>{
-                let labels=new Array();
-                let temperature=new Array();
-                let humidity=new Array();
-                angular.forEach(data.average.reverse(),function(value, key){
-                    labels.push(value.date);
-                    temperature.push(value.temperature.toFixed(2));
-                    humidity.push(value.humidity.toFixed(2));
-                });
-                $scope.labels=labels;
-                $scope.data=[temperature,humidity];
+    var refresh=()=>{
+        $http.get('http://raspberrypi.local:3009/data').success((data, status, headers, config)=>{
+            let labels=new Array();
+            let temperature=new Array();
+            let humidity=new Array();
+            angular.forEach(data.average.reverse(),function(value, key){
+                labels.push(value.date);
+                temperature.push(value.temperature.toFixed(2));
+                humidity.push(value.humidity.toFixed(2));
             });
-        },300000);
-    }
-
+            $scope.labels=labels;
+            $scope.data=[temperature,humidity];
+        });
+    };
+    $interval(refresh,300000)
 }])
 .controller('NameController', ['$scope', function ($scope) {
     $scope.yourName = 'No Name';
