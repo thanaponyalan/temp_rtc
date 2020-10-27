fdc2cf86bba886bc731a3cba7a7adc845f752d43
Thanapon Yalan <thanaponyalan@Thanapons-MacBook-Pro.local>
2020-10-28 04:10:26 +0700

    time format

--- public/tempChart.js
+++ public/tempChart.js
@@ -55,7 +55,7 @@ angular.module("app", ["chart.js","datatables"])
 //.controller('DatatablesCtrl',DatatablesCtrl)
 .filter('formatAsDate',function(){
     return function(dateTime){
-        return moment(dateTime).format('MMMM Do YYYY, h:m a');
+        return moment(dateTime).utc().format('MMMM Do YYYY, HH:mm');
     }
 });
 
